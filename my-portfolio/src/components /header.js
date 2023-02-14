3df0f527a9242bd420e2af8b6a8bf0e6e360e2be
nayul34 <dagg762@gmail.com>
2023-02-14 23:06:20 +0900

    header 추가

--- my-portfolio/src/components /header.js
+++ my-portfolio/src/components /header.js
@@ -1,4 +1,11 @@
+import styled from "styled-components";
 const Header =()=>{
-    
+    return(<SWrapper>
+        <span>김나율 포트폴리오</span>
+    </SWrapper>)
 }
+const SWrapper=styled.div`
+width:100%;
+height:100px;
+border-bottom:2px solid grey;`;
 export default Header
--- my-portfolio/src/pages/Main.js
+++ my-portfolio/src/pages/Main.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
-
+import Header from'../components /header';
 const Main=()=>{
 return(<SContainer>
+    <Header/>
 hi
 </SContainer>)
 }
