24c7bb1c7192f61112499c3eb55fbb97f8b95d86
nayul34 <dagg762@gmail.com>
2023-02-15 23:47:58 +0900

    feat:add header content

--- my-portfolio/src/components /header.js
+++ my-portfolio/src/components /header.js
@@ -1,11 +1,27 @@
 import styled from "styled-components";
 const Header =()=>{
     return(<SWrapper>
+        <SHeaderContatiner>
         <span>김나율 포트폴리오</span>
+        <div className="category">
+        <button>About Me</button>
+        <button>Projects</button>
+        <button>Skills</button>
+        <button>Educations</button>
+        <button>Contact</button>
+        </div>
+       </SHeaderContatiner>
     </SWrapper>)
 }
 const SWrapper=styled.div`
 width:100%;
 height:100px;
-border-bottom:2px solid grey;`;
+border-bottom:2px solid grey;
+display:flex;
+justify-content: center;
+
+`;
+const SHeaderContatiner=styled.div`
+display:flex;
+flex-direction: row;`;
 export default Header
