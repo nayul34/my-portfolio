5e8ed02e0fb94734bcea40c9dedbe6a2cec33859
nayul34 <dagg762@gmail.com>
2023-03-22 23:24:52 +0900

    add project scroll

--- my-portfolio/src/pages/Project.js
+++ my-portfolio/src/pages/Project.js
@@ -1,13 +1,31 @@
 import Team from "../components /Project/Team";
 import Solo from "../components /Project/Solo";
 const Project = () => {
+  const scrollToTeam = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  };
+  const scrollToSolo = () => {
+    window.scrollTo({
+      top: 1040,
+      behavior: "smooth",
+    });
+  };
   return (
     <div className="w-full flex flex-col items-center justify-center">
-      <div className="h-12 w-4/5 bg-gray-300 flex justify-around">
-        <p>Team</p>
-        <p>개인(side project)</p>
+      <div className="h-12 w-4/5 bg-gray-300 flex justify-around items-center sticky top-24">
+        <p className="text-2xl " onClick={scrollToTeam}>
+          Team
+        </p>
+        <p className="text-2xl">/</p>
+        <p className="text-2xl" onClick={scrollToSolo}>
+          Solo
+        </p>
       </div>
       <Team />
+      <hr className=" w-full  "></hr>
       <Solo />
     </div>
   );
