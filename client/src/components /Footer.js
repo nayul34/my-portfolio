cb49f6da766e3fd35853ff70ac8bf8527e3ed930
nayul34 <dagg762@gmail.com>
2023-04-17 23:52:07 +0900

    feat:footer icon hover 효과 추가

--- client/src/components /Footer.js
+++ client/src/components /Footer.js
@@ -5,10 +5,10 @@ const Footer = () => {
     <div className="h-24 text-slate-600 w-full bg-slate-200/25 -mt-12 flex flex-col justify-center items-center relative translate-y-full">
       <div className="flex my_record link ">
         <a href="https://github.com/nayul34" target="_blank" rel="noreferrer">
-          <BsGithub className="m-2.5 cursor-pointer  text-4xl" />
+          <BsGithub className="m-2.5 cursor-pointer hover:text-slate-400 text-4xl" />
         </a>
         <a href="https://velog.io/@dagg762" target="_blank" rel="noreferrer">
-          <SiVelog className="m-2.5 cursor-pointer  text-4xl" />
+          <SiVelog className="m-2.5 cursor-pointer hover:text-slate-400 text-4xl" />
         </a>
       </div>
       <p className=" text-xl">nayul's portfolio</p>
